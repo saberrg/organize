f7d2f27514e20d982908024d5754189c84e71cab
SRG <saberrg@gmail.com>
2025-01-08 18:58:36 -0600

    test

--- touchgrasss/app/ contexts/type/ticketPurchase.ts
+++ touchgrasss/app/ contexts/type/ticketPurchase.ts
@@ -1,5 +1,5 @@
 export interface CreateTicketPayload {
     ticket_type_id: string;
-    add_on_ids: string[];  // Array of event_add_on IDs GAY
+    add_on_ids: string[];  // Array of event_add_on IDs
 }
 
